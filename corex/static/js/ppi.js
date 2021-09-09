5a873c3b8e6661b6c14d27d2bcad4252b2b96752
Mateo Torres <torresmateo@gmail.com>
2021-09-09 16:24:29 -0300

    second color bar

--- corex/static/js/ppi.js
+++ corex/static/js/ppi.js
@@ -131,7 +131,7 @@ class PPI{
             .enter()
             .append('stop')
             //.attr('offset',(d,i) => `${i/(this.color_domain.length - 1) * 100}%`)
-            .attr('offset',(d,i) => `${d/(d3.max(this.drug_color_domain) - d3.min(this.drug_color_domain)) * 100}%`)
+            .attr('offset',(d,i) => `${(d - d3.min(this.drug_color_domain))/(d3.max(this.drug_color_domain) - d3.min(this.drug_color_domain)) * 100}%`)
             .attr('stop-color', d => this.drugColorScale(d));
 
         this.drug_colorbar = this.color_svg.append("rect")
